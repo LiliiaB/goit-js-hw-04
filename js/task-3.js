4024a6871126d0d2640c4f17abf0f11dde7fb48c
LiliiaB <liliya.benatti@gmail.com>
2023-12-02 23:12:34 +0100

    upd task3

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+const profile = {
+  player: { username: "Jacob", playTime: 300 },
+
+  changeUsername(newName) {
+    this.player.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.player.playTime += hours;
+  },
+  getInfo() {
+    return `${this.player.username} has ${this.player.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
